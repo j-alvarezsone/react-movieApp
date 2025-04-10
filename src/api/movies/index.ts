9769d5e3f90d1098d5de48668216af2cdf83e159
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-04-10 15:32:52 +0200

    feat: add Appwrite integration for updating search count and manage movie data

--- src/api/movies/index.ts
+++ src/api/movies/index.ts
@@ -1,10 +1,15 @@
 import type { Movie } from "../../types/movie";
 
 import tmdbApi from "..";
+import { updateSearchCount } from "../../appwrite";
 
 export async function fetchMovies(query: string = "") {
   const endpoint = query ? `/search/movie?query=${encodeURIComponent(query)}` : `/discover/movie?sort_by=popularity.desc`;
   const resp = await tmdbApi.get<Movie>(endpoint);
+
+  if (query && resp.data.results.length > 0) {
+    await updateSearchCount(query, resp.data.results[0]);
+  }
 
   return resp.data;
 }
---
+++ src/appwrite.ts
@@ -0,0 +1,37 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
+import type { Result } from "./types/movie";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export async function updateSearchCount(searchTerm: string, movie: Result) {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal("searchTerm", searchTerm)]);
+
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+    }
+    else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  }
+  catch (error) {
+    throw new Error(error instanceof Error ? error.message : String(error));
+  }
+}
